7262018511099d48b8b59a9839b1a6021fd8a6f2
Clint Simon <me@clintsimon.com>
2013-11-26 11:13:29 -0800

    updates

--- base.js
+++ base.js
@@ -5,12 +5,6 @@ $(document).ready(function() {
 		}
 	});
 
-	var url = window.location.href.toLowerCase();
-
-	if (url == 'http://www.techhike.net/' || url.indexOf('index.html') > 0) {
-		$('.more-posts').removeClass('hide');
-	}
-
 	$('.comments[data-enable="true"]').removeClass('hide');
 	$('.comments.hide').remove()
 });
