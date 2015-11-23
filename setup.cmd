8e64a12a8d862d1bc7d99686189126d3d22c64f0
Clint Simon <me@clintsimon.com>
2015-11-23 14:09:40 -0800

    Updates

--- setup.cmd
+++
@@ -1,3 +0,0 @@
-mkdir public
-git subtree add --prefix=public http://github.com/csim/clintsimon.com.git gh-pages --squash
-git subtree pull --prefix=public http://github.com/csim/clintsimon.com.git gh-pages
