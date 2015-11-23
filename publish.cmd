f51a7a4887cea283422fc4c680d000e7550726bb
Clint Simon <me@clintsimon.com>
2015-11-23 12:17:21 -0800

    Updates

---
+++ publish.cmd
@@ -0,0 +1,10 @@
+rmdir /S /Q public
+
+hugo
+
+xcopy CNAME "public/CNAME"
+
+git add -A
+git commit -m "Updates"
+git push origin master
+git subtree push --prefix=public http://github.com/csim/csim.github.io.git gh-pages
