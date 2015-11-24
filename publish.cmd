b56e34c5c0ef7381d84498113bed2d8de8fbbe5c
Clint Simon <me@clintsimon.com>
2015-11-23 21:56:39 -0800

    Updates

--- publish.cmd
+++ publish.cmd
@@ -5,14 +5,14 @@ hugo
 xcopy /crydif "CNAME" "public"
 
 git add -A
-::git commit -qam "Updates"
+git commit -qam "Updates"
 ::git push origin master
 
 ::git subtree pull --prefix public origin gh-pages
-::git subtree push --prefix public origin gh-pages
+git subtree push --prefix public origin gh-pages
 
 
 
 :: Init subtree
-:: git subtree add --prefix=public http://github.com/csim/clintsimon.com.git gh-pages --squash
-:: git subtree pull --prefix=public http://github.com/csim/clintsimon.com.git gh-pages
+:: git subtree add --prefix public origin gh-pages --squash
+:: git subtree pull --prefix public origin gh-pages
