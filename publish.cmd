1c8209edb2b564936c9d7da99577b08c7c1feadc
Clint Simon <me@clintsimon.com>
2015-11-23 20:30:42 -0800

    cleanup

--- publish.cmd
+++ publish.cmd
@@ -7,7 +7,7 @@ xcopy /crydif "CNAME" "public"
 git add -A
 git commit -m "Updates"
 git push origin master
-git subtree push --prefix=public http://github.com/csim/clintsimon.com.git gh-pages
+git subtree push --prefix public http://github.com/csim/clintsimon.com.git gh-pages
 
 :: Init subtree
 :: git subtree add --prefix=public http://github.com/csim/clintsimon.com.git gh-pages --squash
