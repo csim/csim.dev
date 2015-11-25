a9a73781593b4e68aadcf40b43042f65786019a4
Clint Simon <me@clintsimon.com>
2015-11-24 22:24:33 -0800

    Content Updates

--- publish.cmd
+++ publish.cmd
@@ -4,14 +4,17 @@ hugo
 
 xcopy /crydif "CNAME" "public"
 
-git add -A
-git commit -m "Updates"
-git push origin master
+cd public
+
+git commit -am "Publish"
+git push
+
+cd ..
+git commit -am "Content Updates"
+
 
 ::git subtree pull --prefix public origin gh-pages
-git subtree push --prefix public origin gh-pages --squash
-
-
+::git subtree push --prefix public origin gh-pages --squash
 
 :: Init subtree
 :: git subtree add --prefix public origin gh-pages --squash
