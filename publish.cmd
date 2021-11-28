f7549f9106c5118a24a758080d94551f337818bf
Clint Simon <clint@clintsimon.com>
2021-11-27 19:43:16 -0800

    Content Updates

--- publish.cmd
+++ publish.cmd
@@ -1,18 +1,22 @@
-hugo
-
-xcopy /crydif "CNAME" "public"
-
-cd public
-git checkout gh-pages
-::git pull
-git add -A
-git commit -m "Publish"
-git push --force
-
-cd ..
-git add -A
-git commit -m "Content Updates"
+git commit -am "Content Updates"
 git push
+
+
+::hugo
+
+::xcopy /crydif "CNAME" "public"
+
+:: cd public
+:: git checkout gh-pages
+::git pull
+:: git add -A
+:: git commit -m "Publish"
+:: git push --force
+
+:: cd ..
+:: git add -A
+:: git commit -m "Content Updates"
+:: git push
 
 ::git subtree pull --prefix public origin gh-pages
 ::git subtree push --prefix public origin gh-pages --squash
